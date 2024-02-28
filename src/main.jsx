ba54cb630bc019862fc76cfcca2cf07c8dbef0f4
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-02-28 21:16:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,29 @@
-import React, { Children } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createRoutesFromElements,createBrowserRouter, redirect } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import QuizProcess from './pages/QuizProcess.jsx'
-import SelectDifficulty from './pages/Selectdifficulty.jsx'
-import Question from './pages/Question.jsx'
-import Result from './pages/Result.jsx'
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Route } from 'react-router-dom';
+import { RouterProvider, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import Home from './pages/Home.jsx';
+import QuizProcess from './pages/QuizProcess.jsx';
+import SelectDifficulty from './pages/Selectdifficulty.jsx'; // Corrected import
+import Question from './pages/Question.jsx';
+import Result from './pages/Result.jsx';
 
 const route = createBrowserRouter(
-createRoutesFromElements(
-    <Route path="" element={<App />}>
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
       <Route path="/" element={<Home />} />
-<Route path="quiz_process" element={<QuizProcess/>} />
-<Route path="choose_difficulty" element={<SelectDifficuly/>} />
-<Route path="question" element={<Question/>} />
-
-<Route path="result" element={<Result/>} />   <Route/>
-  ));
+      <Route path="quiz_process" element={<QuizProcess />} />
+      <Route path="choose_difficulty" element={<SelectDifficulty />} /> {/* Corrected component name */}
+      <Route path="question" element={<Question />} /> {/* Corrected component name */}
+      <Route path="result" element={<Result />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <RouterProvider router={route}>
-<App/>
+    <App />
   </RouterProvider>
-
-)
+);
