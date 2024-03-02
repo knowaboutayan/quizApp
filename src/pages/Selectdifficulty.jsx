2abdc334e72db4edda44614446cd93b4fbb0c71a
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-03-02 08:13:17 +0530

    Update Selectdifficulty.jsx

--- src/pages/Selectdifficulty.jsx
+++ src/pages/Selectdifficulty.jsx
@@ -1,9 +1,13 @@
 import { useDispatch } from "react-redux"
+import { useEffect} from "react";
 import { easy, hard, medium } from "../ImagesImport/Images"
 import ChooseTopic from "../components/ChooseTopic"
 import { setQuizDifficulty, setQuizResultShown } from "../reduxTools/slice"
 
 const SelectDifficulty = () => {
+   useEffect(() => {
+  window.scrollTo(0, 0)
+}, [])
   const dispatch = useDispatch()
   const difficultyDetails = [{
     image: easy,
