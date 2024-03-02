9557710e9e1b0f880af4c2285b2c45075d1ddbf9
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-03-02 08:26:09 +0530

    Update PageNotFound.jsx

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ErrorBox from "../Error/ErrorBox";
 import serverError from '../images/serverError.png';
 
-const NotFoundPage = () => {
+const PageNotFound = () => {
   return (
                 <div className="flex center-align">
                 <ErrorBox icon={serverError} errorText="page not found " navigateTo="/" navigateText="back to home" />
@@ -10,4 +10,4 @@ const NotFoundPage = () => {
   );
 };
 
-export default NotFoundPage;
+export default PageNotFound;
