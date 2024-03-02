f244773e4edbc95cb68978c96c70167faf704758
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-03-02 08:12:34 +0530

    Update QuizProcess.jsx

--- src/pages/QuizProcess.jsx
+++ src/pages/QuizProcess.jsx
@@ -1,3 +1,4 @@
+import { useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux"
 import ChooseTopic from "../components/ChooseTopic"
 import * as img from '../ImagesImport/Images'
