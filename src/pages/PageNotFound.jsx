54281fc54ebe4c60b626feeaf669f4a89ebc0d87
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-03-02 08:24:24 +0530

    Update pages

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Erro
+
+const NotFoundPage = () => {
+  return (
+                <div className="flex center-align">
+                <ErrorBox icon={} errorText="page not found " navigateTo="/" navigateText="back to home" />
+            </div>
+  );
+};
+
+export default NotFoundPage;
