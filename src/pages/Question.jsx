39bee1cce88b9866283442333f00f69ba778c56c
AYAN PRADHAN <149263455+knowaboutayan@users.noreply.github.com>
2024-03-02 07:55:14 +0530

    Update Question.jsx

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -30,7 +30,9 @@ const Question = () => {
                 return 'error'
         }
     })()
-    console.log(data)
+   (()=>useEffect(() => {
+  window.scrollTo(0, 0)
+}, [])()
     if (data.length == 0) {
         return <div className="flex center-align"><ErrorBox icon={loading} errorText="please wait...."  ></ErrorBox></div>
     }
